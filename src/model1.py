fb6ced5ffe2e48dadc25770114542072f625fbea
Fazeel-AIML <fasghar40@gmail.com>
2025-02-17 19:28:24 +0500

    Adding Artificats and MlFLOW WORK

---
+++ src/model1.py
@@ -0,0 +1,46 @@
+from sklearn.datasets import load_wine
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+import mlflow
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+
+wine = load_wine()
+X = wine.data
+y = wine.target
+
+x_train, x_test , y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=42)
+
+max_depth=10
+n_estimator =10
+mlflow.set_experiment("RandomForestClassifier")
+with mlflow.start_run():
+    model = RandomForestClassifier(n_estimators=n_estimator,max_depth=max_depth)
+    model.fit(x_train,y_train)
+    
+    y_pred = model.predict(x_test)
+    accuracy = accuracy_score(y_pred=y_pred,y_true=y_test)
+    
+    mlflow.log_metric("accuracy",accuracy)
+    mlflow.log_param("max_depth",max_depth)
+    mlflow.log_param("n_estimator",n_estimator)
+    
+    cm = confusion_matrix(y_pred=y_pred,y_true=y_test)
+    plt.figure(figsize=(10,6))
+    sns.heatmap(cm,annot=True, cmap="Blues",xticklabels=wine.target_names,yticklabels=wine.target_names)
+    plt.ylabel("Actual")
+    plt.xlabel("Predicted")
+    
+    plt.savefig("Confusion-Matrix.png")
+    
+    mlflow.log_artifact("Confusion-Matrix.png")
+    mlflow.log_artifacts(".")
+    mlflow.log_artifact(__file__)
+    
+    mlflow.set_tags({"Author":"Fazeel","Project":"Wine Classification"})
+    mlflow.sklearn.log_model(model,"RandomForest Classifier")
+    print(accuracy)
+    
---
+++ src/test.py
@@ -0,0 +1,7 @@
+import mlflow
+print("Tracking URI Below:")
+print(mlflow.get_tracking_uri())
+print("\n")
+
+
+print(mlflow.get_tracking_uri())
